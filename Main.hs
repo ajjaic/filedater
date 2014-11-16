e4ad70c64b66e99fb7b2287a20c91227adde0832
Aj <btchaggler@gmail.com>
2014-11-16 12:03:36 +0000

    initial working version. prepends modification date to file name

--- Main.hs
+++ Main.hs
@@ -1,1 +1,32 @@
-import
+import System.Environment (getArgs)
+import System.Directory (doesFileExist,
+                            getModificationTime,
+                            renameFile)
+import Control.Monad (filterM)
+import System.Locale (defaultTimeLocale)
+import Data.Time.Format (formatTime)
+import System.FilePath.Posix (splitFileName)
+
+main :: IO ()
+main = do
+    filepaths <- getArgs
+    onlyfiles <- getOnlyFiles filepaths
+    mapM_ renameWithModTime onlyfiles
+
+anomain :: IO ()
+anomain = getArgs
+   >>= (\filepaths -> getOnlyFiles filepaths)
+   >>= mapM_ renameWithModTime
+
+dateFormat :: String
+dateFormat = "%Y_%m_%d"
+
+getOnlyFiles :: [FilePath] -> IO [FilePath]
+getOnlyFiles = filterM doesFileExist
+
+renameWithModTime :: FilePath -> IO ()
+renameWithModTime f = do
+    modtime <- getModificationTime f
+    let timeformat = formatTime defaultTimeLocale dateFormat modtime
+        newfilename = timeformat ++ "_" ++ (snd $ splitFileName f)
+    renameFile f newfilename
